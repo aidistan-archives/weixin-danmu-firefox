c9d0d2324f6e5a5b193567befd358e28ea886380
Aidi Stan <aidistan@live.com>
2015-01-30 21:28:19 +0800

    Update index.html

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 // $(function() {
   var colors = ['#00aeef', '#ea428a', '#f5a70d', '#9962c1', '#333333'];
-  var bullet = $('p.banner-title');
+
+  var baseTop = parseInt($('h1.banner-title').css('top').slice(0, -2));
+  var rangTop = $('.banner-btnbox').css('top').slice(0, -2) - $('h1.banner-title').css('top').slice(0, -2);
 
   // Setup
+  var bullet = $('p.banner-title');
   bullet.css({
     width: bullet.children().width(),
     'text-shadow': '0 0 8px #ffffff'
@@ -13,14 +16,14 @@
   function shoot(bullet) {
     // Reset
     bullet.css({
-      left: window.window.innerWidth,
-      top: 125 + Math.floor(Math.random() * 75),
+      left: window.innerWidth,
+      top: (baseTop + Math.floor(Math.random() * rangTop)),
       color: colors[Math.floor(Math.random() * colors.length)]
     });
     // Animate
     bullet.animate({
         left: -bullet.width()
-      }, ~~(Math.random() * 5000) + 10000, 'linear', function() {
+      }, ~~(Math.random() * 15) + 15000, 'linear', function() {
         shoot(bullet);
       }
     );
