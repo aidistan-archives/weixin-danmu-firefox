9ad6835a87f58940a1d1fbf7c2b150c5b3f716a5
Aidi Stan <aidistan@live.com>
2015-05-22 19:26:12 +0800

    Update helper page

--- data/pages/helper.js
+++ data/pages/helper.js
@@ -1,15 +1,13 @@
 (function(){
   $('#join textarea').keypress(function(e) {
     if (e.key == 'Enter') {
-      $('#join').addClass('cover').addClass('h');
-      $('#join h2').hide();
-      $('#join form').replaceWith('<img src="' + $(this).val() + '" /></div>');
+      $('#join textarea').replaceWith('<img src="' + $(this).val() + '" height=500 /></div>');
     }
   });
 
   $('#thanks div').children().each(function(){ $(this).hide(); });
   $('#thanks div h2').show();
-  $('#thanks a').click(function() {
+  $('#thanks div h2 a').click(function() {
     $('#thanks').removeClass('shout');
     $('#thanks h2').html($('#thanks h2').text());
     $('#thanks div').children().each(function(){ $(this).show(); });
